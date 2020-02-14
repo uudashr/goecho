59e2bc81f1fae9a8dd28d6b03db288878613f0e1
Nuruddin Ashr <uudashr@gmail.com>
2020-02-14 16:03:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# GoEcho
+
+## Installation
+```
+$ go get github.org/uudashr/goecho/cmd/goecho
+```
+
+## Usage
+```
+$ goecho
+Hello
+```
---
+++ cmd/goecho/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/uudashr/goecho"
+)
+
+func main() {
+	fmt.Println(goecho.Echo("Hello"))
+}
